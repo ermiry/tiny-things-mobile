077d832d09c83fa879d582039b697c2d18df54a9
ermiry <erick_salas@ermiry.com>
2020-02-15 12:01:48 -0600

    Added dedicated style files

---
+++ lib/style/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color mainBlue = const Color.fromRGBO(2, 74, 117, 1);
+const Color mainDarkBlue = const Color.fromRGBO(25, 42, 86, 1);
+
+const Color accountFirstColor = const Color(0XFF5F6368);
+// const Color accountFirstColor = const Color.fromRGBO(49, 39, 79, 1);
+const Color accountSecondColor = const Color(0XFFE93B2D);
---
+++ lib/style/style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:pocket/style/colors.dart';
+
+const TextStyle hoursPlayedLabelTextStyle = const TextStyle(
+  fontSize: 16,
+  color: accountFirstColor,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle hoursPlayedTextStyle = const TextStyle(
+  fontSize: 28,
+  color: mainBlue,
+  fontWeight: FontWeight.w800,
+);
+
+const TextStyle infoTitleTextStyle = const TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+  color: mainBlue
+);
+
+const TextStyle infoLabelTextStyle = const TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: mainBlue
+);
+
+const TextStyle infoTextStyle = const TextStyle(
+  fontSize: 16,
+);
