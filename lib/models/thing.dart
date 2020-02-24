506ae9a2a5d05d9bb9d7886a00db452b39f739c7
ermiry <erick_salas@ermiry.com>
2020-02-24 00:45:00 -0600

    Added new things model

---
+++ lib/models/thing.dart
@@ -0,0 +1,27 @@
+class Thing {
+  String title;
+  String content;
+  DateTime date;
+
+  Thing({this.title, this.content, this.date});
+}
+
+final Map<String, int> categories = {
+  'Things': 112,
+  'Work': 58,
+  'Home': 23,
+  'Complete': 31,
+};
+
+final List<Thing> things = [
+  Thing(
+    title: 'Buy ticket',
+    content: 'Buy airplane ticket through Kayak for \$318.38',
+    date: DateTime(2019, 10, 10, 8, 30),
+  ),
+  Thing(
+    title: 'Walk with dog',
+    content: 'Walk on the street with my favorite dog.',
+    date: DateTime(2019, 10, 10, 8, 30),
+  ),
+];
