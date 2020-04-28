321c3ee2a073dc2d3073c1a484daede73efac9d0
ermiry <erick_salas@ermiry.com>
2020-04-28 15:12:31 -0500

    Updated auth provider

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 
-import 'package:http/http.dart' as htpp;
+import 'package:http/http.dart' as http;
 
 import 'package:crypto/crypto.dart';
 import 'package:jwt_decode/jwt_decode.dart';
@@ -16,7 +16,8 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
 
-  Map <String, dynamic> userValues;
+  Map <String, dynamic> _userValues;
+  Map <String, dynamic> get userValues { return this._userValues; }
 
   bool get isAuth {
     return this.token != null;
@@ -46,7 +47,7 @@ class Auth with ChangeNotifier {
     // print(passwordDigest);
 
     try {
-       final res = await htpp.post(url, 
+       final res = await http.post(url, 
         body: {
           'name': name.replaceAll(new RegExp(r'\t'), ''), 
           'username': username.replaceAll(new RegExp(r'\t'), ''), 
@@ -80,7 +81,7 @@ class Auth with ChangeNotifier {
     var passwordDigest = sha256.convert(passwordBytes);
 
     try {
-      final res = await htpp.post(url, 
+      final res = await http.post(url, 
         body: {
           'email': email.replaceAll(new RegExp(r'\t'), ''), 
           'password': passwordDigest.toString(),
@@ -93,7 +94,7 @@ class Auth with ChangeNotifier {
       // print(actualRes);
       this._token = actualRes['token'];
 
-      this.userValues = Jwt.parseJwt(token);
+      this._userValues = Jwt.parseJwt(token);
       // print(this.userValues);
 
       // when we get our token from server, save to memory
@@ -126,7 +127,7 @@ class Auth with ChangeNotifier {
     //   return false;
     // }
 
-    this.userValues = Jwt.parseJwt(token);
+    this._userValues = Jwt.parseJwt(token);
     notifyListeners();
 
     return true;    // success
@@ -138,7 +139,7 @@ class Auth with ChangeNotifier {
      String url = serverURL + '/api/users/forgot';
 
      try {
-      final res = await htpp.post(url, 
+      final res = await http.post(url, 
         body: {
           'email': email.replaceAll(new RegExp(r'\t'), ''), 
         }
@@ -163,6 +164,115 @@ class Auth with ChangeNotifier {
     prefs.clear();    // to delete all saved data
 
     notifyListeners();
+
+  }
+
+  Future <void> changeName(String name) async {
+
+    String url = serverURL + '/api/users/${this._userValues['id']}/name';
+
+    try {
+      final res = await http.post(url, 
+        headers: {
+          // 'Content-type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': token
+        },
+        body: {
+          // 'name': name.replaceAll(new RegExp(r'\t'), ''), 
+          'name': name
+        }
+      );
+
+      if (res.statusCode == 400 || res.statusCode == 500) 
+        throw HttpException (res.body.toString());
+
+      var actualRes = json.decode(res.body);
+      print(actualRes);
+      this._token = actualRes['token'];
+
+      this._userValues = Jwt.parseJwt(token);
+      print(this._userValues);
+
+      // when we get our token from server, save to memory
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('user_token', this._token);
+
+      notifyListeners();
+    }
+
+    catch (error) {
+      throw HttpException (error.toString());
+    }
+
+  }
+
+  Future <void> changePassword(String password, String confirm) async {
+    
+    String url = serverURL + '/api/users/${this._userValues['id']}/password';
+
+    try {
+      password.replaceAll(new RegExp(r'\t'), '');
+      var passwordBytes = utf8.encode(password);
+      var passwordDigest = sha256.convert(passwordBytes);
+
+      confirm.replaceAll(new RegExp(r'\t'), '');
+      var confirmBytes = utf8.encode(confirm);
+      var confirmDigest = sha256.convert(confirmBytes);
+
+      final res = await http.post(url, 
+        headers: {
+          // 'Content-type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': token
+        },
+        body: {
+          'password': passwordDigest.toString(),
+          'password2': confirmDigest.toString()
+        }
+      );
+
+      if (res.statusCode == 400 || res.statusCode == 500) 
+        throw HttpException (res.body.toString());
+
+      var actualRes = json.decode(res.body);
+      print(actualRes);
+      
+
+      notifyListeners();
+    }
+
+    catch (error) {
+      throw HttpException (error.toString());
+    }
+
+  }
+
+  Future <void> delete() async {
+
+    String url = serverURL + '/api/users/';
+
+    try {
+      final res = await http.delete(url, 
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': token
+        }
+      );
+
+      if (res.statusCode == 400) throw HttpException (res.body.toString());
+
+      var actualRes = json.decode(res.body);
+      print(actualRes);
+
+      // after account has been deleted, logout
+      await this.logout();
+    }
+
+    catch (error) {
+      throw HttpException (error.toString());
+    }
 
   }
 
