05e705a4a9b6b36131b5829f9eb0de6a09340811
ermiry <erick_salas@ermiry.com>
2020-05-23 21:53:32 -0500

    added categories to things providers

--- lib/providers/things.dart
+++ lib/providers/things.dart
@@ -1,46 +1,88 @@
 import 'package:flutter/foundation.dart';
 
+class Category {
+
+  final String id;
+
+  final String title;
+  final String description;
+
+  List <Thing> _things = [];
+  List <Thing> get things { return [...this._things]; }
+
+  Category ({
+    @required this.id,
+
+    @required this.title,
+    @required this.description,
+  });
+
+}
+
 class Thing {
 
   final String id;
+
   final String title;
   final String description;
+
+  // Things status:
+  // 0 -> todo
+  // 1 -> in progress
+  // 2 -> done
+  final int status;
 
   final DateTime date;
 
   Thing ({
     @required this.id,
+
     @required this.title,
     @required this.description,
+    @required this.status,
+
     @required this.date
   });
 
 }
 
-final Map <String, int> categories = {
-  'All': 112,
-  'Work': 58,
-  'Home': 23,
-};
-
 class Things with ChangeNotifier {
 
-  List <Thing> _things = [
-    new Thing (id: '1', title: 'This is a test tilte', description: 'This is a test description', date: DateTime.now()),
-    new Thing (id: '2', title: 'This is a test tilte', description: 'This is a test description', date: DateTime.now()),
-    new Thing (id: '3', title: 'This is a test tilte', description: 'This is a test description', date: DateTime.now())
+  List <Category> _categories = [
+    new Category(id: '1', title: 'All', description: 'All'),
+    new Category(id: '2', title: 'Home', description: 'Home')
   ];
 
-  List <Thing> get things { return [..._things]; }
+  List <Category> get categories { return [..._categories]; }
 
-  void addThing(String title, String description) {
-    // this._things.add(new Thing (id: DateTime.now().toString(), title:  title, description: description));
-    // notifyListeners();
-  }
+  int _selectedCategoryIdx = 0;
 
-  void removeThing(String id) {
-    this._things.removeWhere((t) => t.id == id);
+  // int get doors => _doors;
+  // set doors(int numberOfDoors) {
+  //   if(numberOfDoors >= 2 && numberOfDoors <= 6) {
+  //     _doors = numberOfDoors;
+  //   }
+  // }
+
+  int get selectedCategoryIdx => this._selectedCategoryIdx;
+
+  set selectedCategoryIdx(int idx) {
+    this._selectedCategoryIdx = idx;
     notifyListeners();
   }
+
+  // new Thing (id: '1', title: 'This is a test tilte', description: 'This is a test description', status: 0, date: DateTime.now()),
+  // new Thing (id: '2', title: 'This is a test tilte', description: 'This is a test description', status: 0, date: DateTime.now()),
+  // new Thing (id: '3', title: 'This is a test tilte', description: 'This is a test description', status: 0, date: DateTime.now())
+
+  // void addThing(String title, String description) {
+  //   // this._things.add(new Thing (id: DateTime.now().toString(), title:  title, description: description));
+  //   // notifyListeners();
+  // }
+
+  // void removeThing(String id) {
+  //   this._things.removeWhere((t) => t.id == id);
+  //   notifyListeners();
+  // }
 
 }
