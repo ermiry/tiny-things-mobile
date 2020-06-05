8307d6ff39b024e5f4ba3e125017c681a473a3e3
ermiry <erick_salas@ermiry.com>
2020-06-05 15:28:45 -0500

    Added new LabelItem in categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -37,108 +37,6 @@ class _CategoriesScreenState extends State <CategoriesScreen> {
     );
   }
 
-  Widget _label(Label label) {
-    return new Dismissible(
-      key: ValueKey (label.id),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) {
-        var things = Provider.of<Things>(context, listen: false);
-        things.deleteLabel(
-          things.categories[things.selectedCategoryIdx],
-          label.id
-        );
-      },
-      confirmDismiss: (dir) {
-        return showDialog(
-          context: context,
-          builder: (ctx) => AlertDialog (
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12.0))
-            ),
-            title: Text (
-              'Are you sure?', 
-              style: const TextStyle(color: mainDarkBlue, fontSize: 28),
-              textAlign: TextAlign.center,
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text (
-                  'Do you want to delete this label?',
-                  style: const TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-
-                const SizedBox(height: 16),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text ('No', style: const TextStyle(color: mainBlue, fontSize: 18, fontWeight: FontWeight.bold)),
-                      onPressed: () => Navigator.of(ctx).pop(false),
-                    ),
-                    FlatButton(
-                      child: Text ('Okay', style: const TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.bold)),
-                      onPressed: () => Navigator.of(ctx).pop(true),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        );
-      },
-      background: Container (
-        color: Theme.of(context).errorColor,
-        child: Icon (
-          Icons.delete,
-          color: Colors.white,
-          size: 40,
-        ),
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.only(right: 20),
-      ),
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 32),
-        leading: Container(
-          margin: EdgeInsets.only(right: 4, bottom: 4),
-          width: 36,
-          height: 36,
-          decoration: BoxDecoration(
-            color: label.color,
-            borderRadius: BorderRadius.circular(6)
-          ),
-        ),
-        title: new Text(
-          label.title,
-          style: const TextStyle(
-            // color: Colors.black,
-            color: Color(0xFF2F3446),
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        subtitle: new Text(
-          label.description,
-          style: const TextStyle(
-            color: Colors.black54,
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        trailing: Text(
-          Provider.of<Things>(context, listen: false).countLabelThings(label).toString(),
-          style: const TextStyle(
-            color: Color(0xFF0F1426),
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      )
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -243,7 +141,7 @@ class _CategoriesScreenState extends State <CategoriesScreen> {
                       shrinkWrap: true,
                       physics: NeverScrollableScrollPhysics(),
                       itemCount: things.categories[things.selectedCategoryIdx].labels.length,
-                      itemBuilder: (ctx, i) => _label(things.categories[things.selectedCategoryIdx].labels[i])
+                      itemBuilder: (ctx, i) => LabelItem(things.categories[things.selectedCategoryIdx].labels[i])
                     );
                   }
                 ),
@@ -273,6 +171,117 @@ class _CategoriesScreenState extends State <CategoriesScreen> {
           ),
         ],
       ),
+    );
+  }
+
+}
+
+class LabelItem extends StatelessWidget {
+
+  final Label label;
+
+  LabelItem (this.label);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Dismissible(
+      key: ValueKey (label.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        var things = Provider.of<Things>(context, listen: false);
+        things.deleteLabel(
+          things.categories[things.selectedCategoryIdx],
+          label.id
+        );
+      },
+      confirmDismiss: (dir) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog (
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12.0))
+            ),
+            title: Text (
+              'Are you sure?', 
+              style: const TextStyle(color: mainDarkBlue, fontSize: 28),
+              textAlign: TextAlign.center,
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text (
+                  'Do you want to delete this label?',
+                  style: const TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+
+                const SizedBox(height: 16),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text ('No', style: const TextStyle(color: mainBlue, fontSize: 18, fontWeight: FontWeight.bold)),
+                      onPressed: () => Navigator.of(ctx).pop(false),
+                    ),
+                    FlatButton(
+                      child: Text ('Okay', style: const TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.bold)),
+                      onPressed: () => Navigator.of(ctx).pop(true),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        );
+      },
+      background: Container (
+        color: Theme.of(context).errorColor,
+        child: Icon (
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 32),
+        leading: Container(
+          margin: EdgeInsets.only(right: 4, bottom: 4),
+          width: 36,
+          height: 36,
+          decoration: BoxDecoration(
+            color: label.color,
+            borderRadius: BorderRadius.circular(6)
+          ),
+        ),
+        title: new Text(
+          label.title,
+          style: const TextStyle(
+            // color: Colors.black,
+            color: Color(0xFF2F3446),
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: new Text(
+          label.description,
+          style: const TextStyle(
+            color: Colors.black54,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        trailing: Text(
+          Provider.of<Things>(context, listen: false).countLabelThings(label).toString(),
+          style: const TextStyle(
+            color: Color(0xFF0F1426),
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      )
     );
   }
 
