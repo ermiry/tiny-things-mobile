84f790f2e365ac202bf1644c6cab730f33e9656b
ermiry <erick_salas@ermiry.com>
2020-11-06 15:40:52 -0600

    Updated flutter project sources & configuration

--- android/app/src/main/kotlin/com/example/things/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/things/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.ermiry.things
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
