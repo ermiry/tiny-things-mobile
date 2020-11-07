8d2f3b77947ace6dd17cb3a3f8a124521b8e7c68
ermiry <erick_salas@ermiry.com>
2020-11-06 18:11:17 -0600

    Updated auth provider methods

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -14,7 +14,6 @@ import 'package:things/values.dart';
 class Auth with ChangeNotifier {
 
   String _token;
-  DateTime _expiryDate;
 
   Map <String, dynamic> _userValues;
   Map <String, dynamic> get userValues { return this._userValues; }
@@ -23,18 +22,11 @@ class Auth with ChangeNotifier {
     return this.token != null;
   }
 
-  // FIXME: check for date!
-  String get token {
-    if (this._token != null) 
-      // this._expiryDate != null && this._expiryDate.isAfter(DateTime.now()))
-      return this._token;
+  String get token { return this._token; }
 
-    return null;
-  }
-
-  Future <void> signup(String name, String username, String email, String password, String confirm) async {
-
-    String url = serverURL + '/api/users/register';
+  Future <void> signup(
+    String name, String username, String email, String password, String confirm
+  ) async {
 
     password.replaceAll(new RegExp(r'\t'), '');
     var passwordBytes = utf8.encode(password);
@@ -44,24 +36,35 @@ class Auth with ChangeNotifier {
     var confirmBytes = utf8.encode(confirm);
     var confirmDigest = sha256.convert(confirmBytes);
 
-    // print(passwordDigest);
-
     try {
-       final res = await http.post(url, 
+       final res = await http.post(
+        serverURL + '/api/users/register', 
         body: {
           'name': name.replaceAll(new RegExp(r'\t'), ''), 
           'username': username.replaceAll(new RegExp(r'\t'), ''), 
           'email': email.replaceAll(new RegExp(r'\t'), ''), 
           'password': passwordDigest.toString(),
-          'password2': confirmDigest.toString()
+          'confirm': confirmDigest.toString()
         }
       );
 
-      if (res.statusCode == 400) throw HttpException (res.body.toString());
+      switch (res.statusCode) {
+        case 200: {
+          var actualRes = json.decode(res.body);
+          print(actualRes);
+          this._token = actualRes['token'];
 
-      var actualRes = json.decode(res.body);
-      print(actualRes);
-      this._token = actualRes['token'];
+          this._userValues = Jwt.parseJwt(token);
+          // print(this.userValues);
+
+          // when we get our token from server, save to memory
+          final prefs = await SharedPreferences.getInstance();
+          prefs.setString('user_token', this._token);
+        } break;
+
+        default: throw HttpException (res.body.toString()); break;
+      }
+
       notifyListeners();
     }
 
@@ -74,43 +77,58 @@ class Auth with ChangeNotifier {
 
   Future <void> login(String email, String password) async {
 
-    String url = serverURL + '/api/users/login';
-
     password.replaceAll(new RegExp(r'\t'), '');
     var passwordBytes = utf8.encode(password);
     var passwordDigest = sha256.convert(passwordBytes);
 
     try {
-      final res = await http.post(url, 
+      final res = await http.post(
+        serverURL + '/api/users/login', 
         body: {
           'email': email.replaceAll(new RegExp(r'\t'), ''), 
           'password': passwordDigest.toString(),
         }
       );
 
-      if (res.statusCode == 400) throw HttpException (res.body.toString());
+      print(res.body);
 
-      var actualRes = json.decode(res.body);
-      // print(actualRes);
-      this._token = actualRes['token'];
+      switch (res.statusCode) {
+        case 200: {
+          var actualRes = json.decode(res.body);
+          print(actualRes);
 
-      this._userValues = Jwt.parseJwt(token);
-      // print(this.userValues);
+          this._token = actualRes['token'];
 
-      // when we get our token from server, save to memory
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setString('user_token', this._token);
+          this._userValues = Jwt.parseJwt(token);
+          // print(this.userValues);
+
+          // when we get our token from server, save to memory
+          final prefs = await SharedPreferences.getInstance();
+          prefs.setString('user_token', this._token);
+        } break;
+
+        case 400:
+        case 404: {
+          print(res.body);
+          var actualRes = json.decode(res.body);
+          print(actualRes);
+          throw HttpException (actualRes['error']);
+          // throw HttpException ("Failed to authenticate!");
+        } break;
+
+        default: throw HttpException ("Failed to authenticate!"); break;
+      }
 
       notifyListeners();
     }
 
     catch (error) {
+      print(error.toString());
       throw HttpException (error.toString());
     }
 
   }
 
-  // 12/02/2020 -- 20:57 -- load saved auth values
   Future <bool> tryAutoLogin() async {
 
     final prefs = await SharedPreferences.getInstance();
@@ -122,11 +140,6 @@ class Auth with ChangeNotifier {
     // get the token
     this._token = prefs.getString('user_token');
 
-    // FIXME: get the expiry date
-    // if (this._expiryDate.isBefore(DateTime.now())) {
-    //   return false;
-    // }
-
     this._userValues = Jwt.parseJwt(token);
     notifyListeners();
 
@@ -136,16 +149,21 @@ class Auth with ChangeNotifier {
 
   Future <void> recover(String email) async {
 
-     String url = serverURL + '/api/users/forgot';
-
      try {
-      final res = await http.post(url, 
+      final res = await http.post(
+        serverURL + '/api/users/forgot', 
         body: {
           'email': email.replaceAll(new RegExp(r'\t'), ''), 
         }
       );
 
-      if (res.statusCode == 500) throw HttpException (res.body.toString());
+      switch (res.statusCode) {
+        case 200: break;
+
+        default: throw HttpException (res.body.toString()); break;
+      }
+
+      notifyListeners();
     }
 
     catch (error) {
@@ -157,7 +175,6 @@ class Auth with ChangeNotifier {
   Future <void> logout() async {
 
     this._token = null;
-    this._expiryDate = null;
 
     final prefs = await SharedPreferences.getInstance();
     // prefs.remove('user_token');
@@ -169,10 +186,9 @@ class Auth with ChangeNotifier {
 
   Future <void> changeName(String name) async {
 
-    String url = serverURL + '/api/users/${this._userValues['id']}/name';
-
     try {
-      final res = await http.post(url, 
+      final res = await http.post(
+        serverURL + '/api/users/${this._userValues['id']}/name', 
         headers: {
           // 'Content-type': 'application/json',
           'Accept': 'application/json',
@@ -184,19 +200,22 @@ class Auth with ChangeNotifier {
         }
       );
 
-      if (res.statusCode == 400 || res.statusCode == 500) 
-        throw HttpException (res.body.toString());
+      switch (res.statusCode) {
+        case 200: {
+          var actualRes = json.decode(res.body);
+          print(actualRes);
+          this._token = actualRes['token'];
 
-      var actualRes = json.decode(res.body);
-      print(actualRes);
-      this._token = actualRes['token'];
+          this._userValues = Jwt.parseJwt(token);
+          print(this._userValues);
 
-      this._userValues = Jwt.parseJwt(token);
-      print(this._userValues);
+          // when we get our token from server, save to memory
+          final prefs = await SharedPreferences.getInstance();
+          prefs.setString('user_token', this._token);
+        } break;
 
-      // when we get our token from server, save to memory
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setString('user_token', this._token);
+        default: throw HttpException (res.body.toString()); break;
+      }
 
       notifyListeners();
     }
@@ -209,8 +228,6 @@ class Auth with ChangeNotifier {
 
   Future <void> changePassword(String password, String confirm) async {
     
-    String url = serverURL + '/api/users/${this._userValues['id']}/password';
-
     try {
       password.replaceAll(new RegExp(r'\t'), '');
       var passwordBytes = utf8.encode(password);
@@ -220,7 +237,8 @@ class Auth with ChangeNotifier {
       var confirmBytes = utf8.encode(confirm);
       var confirmDigest = sha256.convert(confirmBytes);
 
-      final res = await http.post(url, 
+      final res = await http.post(
+        serverURL + '/api/users/${this._userValues['id']}/password', 
         headers: {
           // 'Content-type': 'application/json',
           'Accept': 'application/json',
@@ -232,12 +250,14 @@ class Auth with ChangeNotifier {
         }
       );
 
-      if (res.statusCode == 400 || res.statusCode == 500) 
-        throw HttpException (res.body.toString());
+      switch (res.statusCode) {
+        case 200: {
+          var actualRes = json.decode(res.body);
+          print(actualRes);
+        } break;
 
-      var actualRes = json.decode(res.body);
-      print(actualRes);
-      
+        default: throw HttpException (res.body.toString()); break;
+      }
 
       notifyListeners();
     }
@@ -250,10 +270,9 @@ class Auth with ChangeNotifier {
 
   Future <void> delete() async {
 
-    String url = serverURL + '/api/users/';
-
     try {
-      final res = await http.delete(url, 
+      final res = await http.delete(
+        serverURL + '/api/users', 
         headers: {
           'Content-type': 'application/json',
           'Accept': 'application/json',
@@ -261,13 +280,17 @@ class Auth with ChangeNotifier {
         }
       );
 
-      if (res.statusCode == 400) throw HttpException (res.body.toString());
+      switch (res.statusCode) {
+        case 200: {
+          var actualRes = json.decode(res.body);
+          print(actualRes);
 
-      var actualRes = json.decode(res.body);
-      print(actualRes);
+          // after account has been deleted, logout
+          await this.logout();
+        } break;
 
-      // after account has been deleted, logout
-      await this.logout();
+        default: throw HttpException (res.body.toString()); break;
+      }
     }
 
     catch (error) {
